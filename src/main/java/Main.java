ec4a9d5f242469145bc75163352394e18a4dd7e2
lukaszkuzmiak <162459802+lukaszkuzmiak@users.noreply.github.com>
2024-03-20 13:52:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[5];
+        System.out.println("Podaj 5 liczb:");
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = input.nextInt();
+        }
+        input.close();
+        int min = numbers[0]; // Assume the first number is the smallest
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i]; // Update min if a smaller number is found
+            }
+        }
+        System.out.println("Najmniejsza liczba to: " + min);
+    }
+}
